649046c49babee9f3faa96eafb36ccc800e9d5db
ManyakRus <30662875+ManyakRus@users.noreply.github.com>
2021-11-16 17:09:19 +0300

    1

--- build.bat
+++ build.bat
@@ -7,5 +7,6 @@ ren main.exe %FileName%.exe
 copy %FileName%.exe %FileName%_ready\%FileName%.exe
 copy readme.txt %FileName%_ready\readme.txt
 copy readme.md %FileName%_ready\readme.md
+copy settings.txt %FileName%_ready\settings.txt
 
 "C:\Program Files\7-Zip\7z.exe" a -tzip %FileName%_ready %FileName%_ready 
